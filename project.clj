2901e897f6cf987033e0b40bd703fdb9356203ff
Craig B. Ludington <me@clojure.us>
2017-12-07 14:21:52 -0600

    add license

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject solpp "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "developer tools for working with Solidity source code"
+  :url "http://alpheus.me"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
